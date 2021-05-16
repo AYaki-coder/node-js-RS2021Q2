3e78a2bf6b8d23fddf9f7460f1d49ee8c4ac3ca7
ayaki-coder <sj.a.yakimovich@gmail.com>
2021-05-16 22:51:28 +0300

    feat: add users endpoints

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,35 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+const DB = require('../../common/inMemoryDB');
+
+const getAll = async () => DB.getAllUsers();
+// TODO: mock implementation. should be replaced during task development
+
+const get = async id => {
+  const user = await DB.getUser(id);
+
+  if (!user) {
+    throw new Error(`The User with id ${id} was not found.`);
+  }
+
+  return user;
 };
 
-module.exports = { getAll };
+const create = async user => DB.createUser(user);
+
+const update = async (id, data) => {
+  const user = await DB.updateUser(id, data);
+  if (!user) {
+    throw new Error(`The User with id ${id} was not found.`);
+  }
+  return user;
+};
+
+const remove = async id => {
+  const user = await DB.removeUser(id);
+
+  if (!user) {
+    throw new Error(`The User with id ${id} was not found.`);
+  }
+
+  return user;
+};
+module.exports = { getAll, get, create, remove, update };
